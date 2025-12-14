efbdf4a7d76e0e8e5e7b329fccbcace8c3349fb2
Polack92400 <romain.polack75@gmail.com>
2025-12-14 02:48:38 +0100

    Add FAT32 boot sector parsing

--- src/lib.rs
+++ src/lib.rs
@@ -53,3 +53,43 @@ impl<'a> DeviceBlock for RamDisk<'a> {
         self.block_size
     }
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct BootSector {
+    pub bytes_per_sector: u16,
+    pub sectors_per_cluster: u8,
+    pub reserved_sectors: u16,
+    pub fat_count: u8,
+    pub fat_size_sectors: u32,
+    pub root_cluster: u32,
+}
+
+impl BootSector {
+    pub fn read_from<D: DeviceBlock>(device: &D) -> Result<Self, DeviceError> {
+        let mut block = [0u8; 512];
+
+        if device.block_size() != 512 {
+            return Err(DeviceError::ReadFailed);
+        }
+
+        device.read(0, &mut block)?;
+
+        let bytes_per_sector = u16::from_le_bytes([block[0x0B], block[0x0C]]);
+        let sectors_per_cluster = block[0x0D];
+        let reserved_sectors = u16::from_le_bytes([block[0x0E], block[0x0F]]);
+        let fat_count = block[0x10];
+        let fat_size_sectors =
+            u32::from_le_bytes([block[0x24], block[0x25], block[0x26], block[0x27]]);
+        let root_cluster =
+            u32::from_le_bytes([block[0x2C], block[0x2D], block[0x2E], block[0x2F]]);
+
+        Ok(Self {
+            bytes_per_sector,
+            sectors_per_cluster,
+            reserved_sectors,
+            fat_count,
+            fat_size_sectors,
+            root_cluster,
+        })
+    }
+}
